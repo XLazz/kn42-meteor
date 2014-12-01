c993460fb9f0046d26a1d8106594b8cd5babec34
Stan Podolski <stan@podolski.org>
2014-12-01 04:04:02 -0600

    Added experience part. Now it saves and returns

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,31 +1,87 @@
 var FEATURED_COUNT = 4;
 
-Template.home.helpers({
+findExperiences = function(){
+	var services = Services.findOne({
+		place_id: Session.get('currentPlace')
+	});
+	if (services) {
+		console.log('experiences ', services);
+		return services.experiences;
+	}
+	var currentPlace = Places.findOne({place_id: Session.get('currentPlace')});
+	if (currentPlace){
+		console.log('experiences ', currentPlace.types);
+		return currentPlace.types;
+	}
+}
+
+Template.home.helpers({	
+  // selects FEATURED_COUNT number of recipes at random
+  featuredRecipes: function() {
+    var recipes = _.values(RecipesData);
+    var selection = [];
+    
+    for (var i = 0;i < FEATURED_COUNT;i++)
+      selection.push(recipes.splice(_.random(recipes.length - 1), 1)[0]);
+    return selection;
+  },
+  
+  activities: function() {
+    return Activities.latest();
+  },
+  
+  latestNews: function() {
+    return News.latest();
+  },
+});
+
+Template.home.events({
+	/* 'click .change': function (event, template) {
+		var place_id = $(event.currentTarget).attr("id");
+		Session.set('radius', 50);
+		console.log('chenging place ', place_id);
+//		var userLocationId = $(event.currentTarget).attr("id");
+//		Meteor.call('getLocations','list');
+
+		return Session.set('changeplace', true);
+	},
+	
+	'click .confirm': function (event, template) {
+//		var userLocationId = $(event.currentTarget).attr("id");
+//		Meteor.call('getLocations','list');
+		var place_id = $(event.currentTarget).attr("id");
+		var myPlace = MerchantsCache.findOne({place_id: place_id}, {fields: {_id: 0}});		
+		var myId = Places.findOne({place_id: place_id}, {fields: {_id: 1}});		
+		console.log('confirming place_id ', place_id, myPlace, myId );
+		myPlace.confirmed = 1;
+		if (myId) {
+			Places.upsert(
+				{_id: myId._id}, 
+				{
+					$set: myPlace
+				}
+			);		
+		} else {
+			Places.insert(
+				{
+					$set: myPlace
+				}
+			);					
+		}
+			
+		return Session.set('changeplace', false);
+	},
+
+	"click #experience": function (event, template) {
+		Overlay.show('selectExperience');	
+	},	 */
+});
+
+Template.homeinside.helpers({
 	currentplaces: function(){
 		if (!Meteor.userId()) {return;};
 //		if (!Session.get('changeplace')) {return};
-		var lastPlaces;
-/* 		var currentresults = Session.get('gotPlaces');
-		var google_places = currentresults.google_places.results; */
-//		var lastGeoLog = GeoLog.find({userId: Meteor.userId(), place_id: {$not: {$size: 0}}}, {sort: {timestamp: -1}}, {limit: 1}).fetch()[0];
-/* 		var lastGeoLog = UserLocations.findOne({userId: Meteor.userId(), place_id: {$not: {$size: 0}}}, {sort: {started: -1}});
-		if (!lastGeoLog) {
-			return {name: 'unknown'};
-		}
-		console.log('home currentplaces lastGeoLog ', lastGeoLog);
-		var place_id = lastGeoLog.place_id;	
-		Session.set('userLocationId', lastGeoLog.user_history_location_id); */
-/* 		lastPlaces = Places.findOne(
-			{place_id: 
-				function () {
-						var lastGeoLog = UserLocations.findOne({userId: Meteor.userId(), place_id: {$not: {$size: 0}}}, {sort: {started: -1}});
-						Session.set('userLocationId', lastGeoLog.user_history_location_id);
-						console.log('home currentplaces lastGeoLog ', lastGeoLog);
-						return lastGeoLog.place_id;
-				}
-			}
-		) */;
-		
+		var lastPlaces;		
 		if (!lastPlaces) {
 			return UserLocations.findOne({
 				userId: Meteor.userId(), place_id: {$not: {$size: 0}}
@@ -56,44 +112,47 @@ Template.home.helpers({
 	},	
 	
 	placeconfirmed: function(){
-		currentPlace = Places.findOne({place_id: Session.get('currentPlace')});
-		if (currentPlace){
-			experiences = currentPlace.types;
-		}
-		console.log('experiences ', experiences);
-		return experiences;
+		currentPlace = Places.findOne({place_id: Session.get('currentPlace'), confirmed: 1});
+		console.log('experiences placeconfirmed ', currentPlace.place_id);
+		return currentPlace;
 	},
 	
 	status: function(){
 		if (!Meteor.userId()) {return;};
 		var lastGeoLog = GeoLog.findOne({userId: Meteor.userId(), place_id: {$not: {$size: 0}}}, {sort: {timestamp: -1}});
+		if (!lastGeoLog.status) {
+			if (!Session.get('geoback')) {
+				return 'service is not running';
+			} else {
+				return 'location can not be updated';
+			}
+		}
 		return lastGeoLog.status;	
 	},	
-	
-  // selects FEATURED_COUNT number of recipes at random
-  featuredRecipes: function() {
-    var recipes = _.values(RecipesData);
-    var selection = [];
-    
-    for (var i = 0;i < FEATURED_COUNT;i++)
-      selection.push(recipes.splice(_.random(recipes.length - 1), 1)[0]);
-    return selection;
-  },
-  
-  activities: function() {
-    return Activities.latest();
-  },
-  
-  latestNews: function() {
-    return News.latest();
-  },
-	
+
 	changeplace: function(){
 		return Session.get('changeplace');
+	},
+	
+	showexpselect: function(){
+		return Session.get('showexp');
+	},
+	
+	exp1: function(){
+		$('#input-tags').selectize({
+				delimiter: ',',
+				persist: false,
+				create: function(input) {
+						return {
+								value: input,
+								text: input
+						}
+				}
+		});	
 	}
 });
 
-Template.home.events({
+Template.homeinside.events({
 	'click .change': function (event, template) {
 		var place_id = $(event.currentTarget).attr("id");
 		Session.set('radius', 50);
@@ -129,4 +188,46 @@ Template.home.events({
 			
 		return Session.set('changeplace', false);
 	},
+
+	"click .expselect": function (event, template) {
+		Session.set('showexp', true);
+	},	
+});
+
+Template.selectExperience.helpers({
+	experiences: function(){
+		var services = Services.find({
+			place_id: Session.get('currentPlace')
+		});
+		console.log('experiences Services ', services.fetch());
+		if (services.count()) {
+			return services.fetch();
+		}
+		var currentPlace = Places.findOne({place_id: Session.get('currentPlace')});
+		console.log('experiences Places ', currentPlace.types);
+		if (currentPlace){		
+			return currentPlace.types;
+		}
+	},
+});
+
+Template.selectExperience.events({
+	'submit form': function(event){
+    event.preventDefault();
+		var experiences = event.target.experience.value;
+		var experiences = $.csv.toArray(experiences);
+		console.log('experiences selectExperience.events ', experiences);
+		experiences.forEach(function (item, index, array){
+			if (item){
+				if	(!Services.findOne({place_id: Session.get('currentPlace'), experience: item})) {
+					Services.insert({
+						experience: item,
+						place_id: Session.get('currentPlace')
+					});
+				}
+			}
+		});
+		return experiences;
+	},
+
 });
