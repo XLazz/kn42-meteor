269c188717fee181f2c9b4ee453bae0eac39639e
Stan Podolski <stan@podolski.org>
2015-02-19 12:34:29 -0600

    deleted ignored files

--- build.sh
+++
@@ -1,5 +0,0 @@
-#meteor build ../build-dev --server=kn42dev.meteor.com
-cd ../build-dev/android/
-jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 unaligned.apk Kn42
-rm production.apk
-/home/ipstas/.meteor/android_bundle/android-sdk/build-tools/21.0.0/zipalign 4 unaligned.apk kn42-dev-met.apk
